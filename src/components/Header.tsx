4182c9e4f27aef8cb90ef2a38a145c84c4a8bacb
Filipe Kohls <filiperenank@gmail.com>
2021-11-18 21:27:21 -0300

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import { Box, Flex, Button, useDisclosure, Image } from '@chakra-ui/react';
+
+
+export function Header(): JSX.Element {
+  const { onOpen, isOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box bgColor="pGray.800">
+        <Flex
+          justifyContent="space-between"
+          alignItems="center"
+          maxW={1120}
+          mx="auto"
+          px={20}
+          py={6}
+        >
+          <Image src="logo.svg" h={10} />
+          <Button onClick={() => onOpen()}>Adicionar imagem</Button>
+        </Flex>
+      </Box>
+
+    </>
+  );
+}
